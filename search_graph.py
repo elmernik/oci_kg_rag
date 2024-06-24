da853dd5042f6a80b341153c0a4ff54350dbbd37
elmernik <113885272+elmernik@users.noreply.github.com>
2024-06-24 14:35:31 +0300

    implement function to get next nodes from particular node

--- search_graph.py
+++ search_graph.py
@@ -5,22 +5,17 @@ from show_graph import show_graph
 from config_private import user, pwd, dsn, wloc, wpwd
 
 
-def search_graph(query, get_related_to_object1, get_related_to_object2):
+def vector_search_graph(query, top_n=5):
+    """Function for searching the graph using vector similarity"""
     # Embed query
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-l6-v2")
     embed_query = array.array("f", embeddings.embed_query(query))
 
     # Define SQL
-    vec_search_sql = '''SELECT OBJECT_1, RELATION, OBJECT_2 
+    vec_search_sql = f'''SELECT OBJECT_1, RELATION, OBJECT_2 
         FROM KG 
         ORDER BY VECTOR_DISTANCE(vec, :query_vector, COSINE) 
-        FETCH EXACT FIRST 5 ROWS ONLY'''
-
-    object1_sql = '''SELECT OBJECT_1, RELATION, OBJECT_2 
-        FROM KG WHERE OBJECT_1 = :query_object'''
-
-    object2_sql = '''SELECT OBJECT_1, RELATION, OBJECT_2 
-        FROM KG WHERE OBJECT_2 = :query_object'''
+        FETCH EXACT FIRST {top_n} ROWS ONLY'''
 
     # Connect to database
     connection = oracledb.connect(user=user, password=pwd, dsn=dsn,
@@ -35,34 +30,62 @@ def search_graph(query, get_related_to_object1, get_related_to_object2):
         print(row)
         result.append(list(row))
 
-    # Get everything else immediately related to each object 1 !!! TODO This logic should be improved !!!
-    if get_related_to_object1:
-        object1s = set([i[0] for i in result])
-        for o1 in object1s:
-            for row in cursor.execute(object1_sql, [o1]):
-                result.append(list(row))
-
-    # Get everything else immediately related to each object 2 !!! TODO This logic should be improved !!!
-    if get_related_to_object2:
-        object2s = set([i[2] for i in result])
-        for o2 in object2s:
-            for row in cursor.execute(object2_sql, [o2]):
-                result.append(list(row))
-
     connection.close()
     return result
+
+
+def get_next_nodes(start_node, depth=1, parent_or_child="child"):
+    """Function for getting the next nodes (either parent or child)
+    from a specific starting node until a certain depth.
+    """
+    # Default to child
+    poc = 1 if parent_or_child == "child" else (2 if parent_or_child == "parent" else 1)
+
+    # Define sql query depending on whether you want parent or child nodes
+    get_nodes_sql = f'''SELECT OBJECT_1, RELATION, OBJECT_2 
+        FROM KG WHERE OBJECT_{poc} = :query_object'''
+
+    # Connect to database
+    connection = oracledb.connect(user=user, password=pwd, dsn=dsn,
+                                wallet_location=wloc, wallet_password=wpwd)
+    cursor = connection.cursor()
+
+    def _fetch_nodes(cursor, current_node, depth, current_depth, sql, poc):
+        """Recursive helper function for fetching the nodes"""
+        # If depth has been reached then stop
+        if current_depth > depth:
+            return []
+        
+        # Get next nodes
+        cursor.execute(sql, [current_node])
+        results = cursor.fetchall()
+
+        all_results = results[:]
+
+        # Recursion on the next node
+        for next_parent, _, next_child in results:
+            if poc == 1:
+                all_results.extend(_fetch_nodes(cursor, next_child, depth, current_depth + 1, sql, poc))
+            else:
+                all_results.extend(_fetch_nodes(cursor, next_parent, depth, current_depth + 1, sql, poc))
+        return all_results
+    
+    # Call helper function and return result from recursion
+    try:
+        next_nodes = _fetch_nodes(cursor, start_node, depth, 1, get_nodes_sql, poc)
+    finally:
+        connection.close()
+    
+    return next_nodes
 
 
 if __name__ == "__main__":
     # Define query
     question = "Who is Warren Buffets father?"
 
-    # Define if you want to get other nodes
-    gro1 = False
-    gro2 = False
-
     # Get result
-    result = search_graph(question, gro1, gro2)
+    #result = search_graph(question, gro1, gro2)
+    result = get_next_nodes("businessman", 1, "parent")
 
     # Show a partial knowledge graph
     head, relation, tail = list(zip(*result))
