5959a8a58b67e202681bb05be6a6a54c2276c9b3
elmernik <113885272+elmernik@users.noreply.github.com>
2024-06-27 19:49:07 +0300

    alter Knowledge Graph table to work with LangChain

--- insert_kg_data.py
+++ insert_kg_data.py
@@ -19,17 +19,19 @@ with open(file_path, "r") as f:
    lines = [[item.strip("'") for item in line.strip("[]\n").split("', '")] for line in f.readlines()]
    unzipped = list(zip(*lines))
    head, relation, tail = unzipped[0], unzipped[1], [i.strip("'],") for i in unzipped[2]]
+   texts = [" ".join(i) for i in list(zip(head, relation, tail))]
+
 
 # Embed each object1-relation-object2 into a vector
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-l6-v2")
-vectors = [array.array("f", embeddings.embed_query(" ".join(line))) for line in lines]
+vectors = [array.array("f", embeddings.embed_query(text)) for text in texts]
 
 # Generate IDs
 ids = [i for i in range(1, len(lines)+1)]
 
 # Insert data
-data = list(zip(*[ids, list(head), list(relation), list(tail), vectors]))
-cursor.executemany("INSERT INTO KG VALUES (:1, :2, :3, :4, :5)", data)
+data = list(zip(*[ids, texts, vectors, list(head), list(relation), list(tail)]))
+cursor.executemany("INSERT INTO KG(ID, TEXT, EMBEDDING, OBJECT_1, RELATION, OBJECT_2) VALUES (:1, :2, :3, :4, :5, :6)", data)
 
 # Commit and close connection
 connection.commit()
